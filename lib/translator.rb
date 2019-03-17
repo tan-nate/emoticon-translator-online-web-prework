a19f4e4caa91c5abd232ac7e2ea00eb606c429f3
Nate Tan <nktan93@gmail.com>
2019-03-17 03:17:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,7 +18,7 @@ def get_japanese_emoticon(file_path, english_emoticon)
   if load_library(file_path)["get_emoticon"].keys.include?(english_emoticon)
     load_library(file_path)["get_emoticon"][english_emoticon]
   else
-    ""
+    "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning
