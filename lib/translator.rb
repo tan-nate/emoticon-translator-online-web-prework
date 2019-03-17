a0237dd2f141ca4f2abc707e18f4aebe1fdb7b5c
Nate Tan <nktan93@gmail.com>
2019-03-17 02:43:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,8 @@
-# require modules here
+require "yaml"
+require "pry"
 
-def load_library
-  # code goes here
+def load_library(file_path)
+  YAML.load_file(file_path)
 end
 
 def get_japanese_emoticon
@@ -11,3 +12,5 @@ end
 def get_english_meaning
   # code goes here
 end
+
+#binding.pry
