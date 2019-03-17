f4df403df7f96db4d3b6d0421bceacf26d85fc89
Nate Tan <nktan93@gmail.com>
2019-03-17 03:02:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,8 +6,11 @@ def load_library(file_path)
   new_hash = {}
   new_hash["get_meaning"] = {}
   new_hash["get_emoticon"] = {}
-  file.each do |meaning, emoticons_hash|
-    
+  file.each do |meaning, emoticons_array|
+    new_hash["get_meaning"][emoticons_array[2]] = meaning
+    new_hash["get_emoticon"][emoticons_array[1]] = [emoticons_array[2]]
+  end
+  new_hash
 end
 
 def get_japanese_emoticon
